4436710db923b19d999c0a43dc286576da5e6e2c
neo <liang.gong@gmail.com>
2015-10-09 23:54:05 +0800

    1.0.0 Abstract Iterator ArrayIterator

--- test/testIterator.js
+++ test/testIterator.js
@@ -1,35 +1,34 @@
 /**
  * Created by neo on 2015/10/7.
  */
-var should=require("should");
+var should = require("should");
 
 var Iterators = require('../index');
-var Iterator=Iterators.Iterator;
-describe("InstanceOf", function () {
+var Iterator = Iterators.Iterator;
+describe("Abstract Iterator Behavior", function () {
     it("instance should be Iterator", function () {
-        var it1=new Iterator();
+        var it1 = new Iterator();
         it1.should.be.an.instanceof(Iterator);
     });
-});
-describe("Property", function () {
+
     it("it1 should have property current,eof", function () {
-        var it1=new Iterator();
+        var it1 = new Iterator();
         it1.should.have.property("current");
         it1.should.have.property("eof");
     });
+
+    it("reset ", function () {
+        var it1 = new Iterator();
+        should.equal(it1.current, null);
+        should.equal(it1.eof, true);
+    });
+    it("next ", function () {
+        var it1 = new Iterator();
+        should.equal(it1.next(), null);
+    });
+    it("hasNext ", function () {
+        var it1 = new Iterator();
+        should.equal(it1.hasNext(), false);
+    });
 });
-describe("Behave",function(){
-    it("reset ",function(){
-        var it1=new Iterator();
-        should.equal(it1.current,null);
-        should.equal(it1.eof,true);
-    })
-    it("next ",function(){
-        var it1=new Iterator();
-        should.equal(it1.next(),null);
-    })
-    it("hasNext ",function(){
-        var it1=new Iterator();
-        should.equal(it1.hasNext(),false);
-    })
-})
+
